bd0bd015e16ff0e6041c4bb3a3b341e177cb37b1
Pascal Diehm <pdiehm8@gmail.com>
2026-01-29 21:52:43 +0100

    feat(networking): add google DNS servers

--- base/common/networking.nix
+++ base/common/networking.nix
@@ -27,6 +27,10 @@
       "1.0.0.1#one.one.one.one"
       "2606:4700:4700::1111#one.one.one.one"
       "2606:4700:4700::1001#one.one.one.one"
+      "8.8.8.8#dns.google"
+      "8.8.4.4#dns.google"
+      "2001:4860:4860::8888#dns.google"
+      "2001:4860:4860::8844#dns.google"
     ];
   };
 

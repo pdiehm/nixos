e37f9e660b655b0e786cd057cbb4f0c1d2aae0cc
Pascal Diehm <pdiehm8@gmail.com>
2026-02-23 14:16:51 +0100

    ci(upgrade): use cargo-edit for rust dependency upgrade

--- apps/upgrade.sh
+++ apps/upgrade.sh
@@ -17,8 +17,7 @@ echo "::endgroup::"
 
 echo "::group::Dynhostmgr"
 pushd overlay/dynhostmgr
-cargo update -Z unstable-options --breaking # HACK: https://github.com/rust-lang/cargo/issues/12425
-cargo update
+cargo upgrade --incompatible
 popd
 
 CHANGES="$(git diff overlay/dynhostmgr/Cargo.toml)"
@@ -32,8 +31,7 @@ echo "::endgroup::"
 
 echo "::group::Waybar UPS plugin"
 pushd overlay/waybar-ups
-cargo update -Z unstable-options --breaking # HACK: https://github.com/rust-lang/cargo/issues/12425
-cargo update
+cargo upgrade --incompatible
 popd
 
 CHANGES="$(git diff overlay/waybar-ups/Cargo.toml)"

f20f968b0154909d393c3530137b7fb3102d69c1
Pascal Diehm <pdiehm8@gmail.com>
2026-03-02 20:24:04 +0100

    feat(nvim): handle comma in brackets like semicolon

--- resources/vim/nvim.lua
+++ resources/vim/nvim.lua
@@ -4,16 +4,18 @@ vim.keymap.del("n", "grn")
 vim.keymap.del("n", "grr")
 vim.keymap.del("n", "grt")
 
-vim.keymap.set("i", ";", function()
-  local line = vim.api.nvim_get_current_line()
-  local col = vim.api.nvim_win_get_cursor(0)[2]
+for _, key in pairs({ ";", "," }) do
+  vim.keymap.set("i", key, function()
+    local line = vim.api.nvim_get_current_line()
+    local col = vim.api.nvim_win_get_cursor(0)[2]
 
-  if vim.tbl_contains({ "()", "[]", "{}" }, line:sub(col, col + 1)) then
-    return "<Esc>la;<Esc>hi"
-  else
-    return ";"
-  end
-end, { expr = true, noremap = true })
+    if vim.tbl_contains({ "()", "[]", "{}" }, line:sub(col, col + 1)) then
+      return "<Esc>la" .. key .. "<Esc>hi"
+    else
+      return key
+    end
+  end, { expr = true, noremap = true })
+end
 
 vim.lsp.buf.empty_rename = function()
   vim.ui.input({ prompt = "New Name: " }, function(name)

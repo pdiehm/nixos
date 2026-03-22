c8749b889168e8e9f04d42e47179adbff1ad2573
Pascal Diehm <pdiehm8@gmail.com>
2026-03-22 15:12:15 +0100

    feat(nvim): enable blink-cmp for commandline

--- base/desktop/nvim.nix
+++ base/desktop/nvim.nix
@@ -190,8 +190,6 @@
           enable = true;
 
           settings = {
-            cmdline.enabled = false;
-            fuzzy.sorts = [ "exact" "score" "sort_text" ];
             signature.enabled = true;
 
             completion = {
